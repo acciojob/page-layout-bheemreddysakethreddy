bddaa508b6b45da7eab877654684d57ec2e503d0
bheemreddysakethreddy <sakethbheem@gmail.com>
2026-01-04 11:35:09 +0530

    final change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 
 import React from "react";
 import './../styles/App.css';
+import PageLayout from "./PageLayout";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <PageLayout header={"Welcomr to my website"} footer={"c 2023 My Website. All rights reserverd"}/>
     </div>
   )
 }
